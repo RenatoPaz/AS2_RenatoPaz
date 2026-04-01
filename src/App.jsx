c0f96f5153424d09f52273ee7563763e5e912fb6
RenatoPaz <renatogomesdapaz01@gmail.com>
2026-03-31 22:19:44 -0600

    Add course name

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ function App() {
   return (
     <div>
       <h1>Renato Gomes da Paz</h1>
+      <h2>CICD Pipeline with React</h2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
